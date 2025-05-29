4320d28134b6d9833e5c2a741065dc830d4503ce
KAS <krishaang_1808@hotmail.com>
2025-05-30 00:06:13 +0530

    improve: enhance stringify formatting for arrays and objects
    - Use Node.js-style array formatting with 12 items per line
    - Improve object formatting with proper indentation
    - Handle nested structures consistently
    - Add smart line breaking for better readability
    - Fix circular reference handling
    - Optimize output for both simple and complex structures

--- helpers/stringify.go
+++ helpers/stringify.go
@@ -3,17 +3,111 @@ package helpers
 import (
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/dev-kas/virtlang-go/v3/shared"
 )
 
+const (
+	maxLineLength = 80
+	itemsPerLine = 12
+)
+
 func Stringify(value shared.RuntimeValue, internal bool) string {
-	return stringifyWithVisited(value, internal, make(map[uintptr]bool))
+	return stringifyWithVisited(value, internal, make(map[uintptr]bool), 0)
 }
 
-func stringifyWithVisited(value shared.RuntimeValue, internal bool, visited map[uintptr]bool) string {
-	output := ""
+func formatObject(obj map[string]*shared.RuntimeValue, internal bool, visited map[uintptr]bool, indentLevel int) string {
+	if len(obj) == 0 {
+		return "{}"
+	}
 
+	indent := strings.Repeat("  ", indentLevel)
+	nextIndent := strings.Repeat("  ", indentLevel+1)
+	var items []string
+
+	for key, val := range obj {
+		item := fmt.Sprintf("%s: %s", key, stringifyWithVisited(*val, internal, visited, indentLevel+1))
+		items = append(items, item)
+	}
+
+	oneLine := "{ " + strings.Join(items, ", ") + " }"
+	if len(oneLine) <= maxLineLength {
+		return oneLine
+	}
+
+	return "{\n" + nextIndent + strings.Join(items, ",\n"+nextIndent) + "\n" + indent + "}"
+}
+
+func isSimpleValue(value shared.RuntimeValue) bool {
+	switch value.Type {
+	case shared.Number, shared.Boolean, shared.Nil, shared.String:
+		return true
+	default:
+		return false
+	}
+}
+
+func formatArray(arr []shared.RuntimeValue, internal bool, visited map[uintptr]bool, indentLevel int) string {
+	if len(arr) == 0 {
+		return "[]"
+	}
+
+	indent := strings.Repeat("  ", indentLevel)
+	nextIndent := strings.Repeat("  ", indentLevel+1)
+
+	allSimple := true
+	for _, item := range arr {
+		if !isSimpleValue(item) {
+			allSimple = false
+			break
+		}
+	}
+
+	if allSimple {
+		singleLine := "[ "
+		for i, item := range arr {
+			if i > 0 {
+				singleLine += ", "
+			}
+			singleLine += stringifyWithVisited(item, internal, visited, 0)
+		}
+		singleLine += " ]"
+
+		if len(singleLine) <= maxLineLength {
+			return singleLine
+		}
+
+		var lines []string
+		for i := 0; i < len(arr); i += itemsPerLine {
+			end := i + itemsPerLine
+			if end > len(arr) {
+				end = len(arr)
+			}
+			
+			var lineItems []string
+			for j := i; j < end; j++ {
+				lineItems = append(lineItems, stringifyWithVisited(arr[j], internal, visited, 0))
+			}
+			
+			line := nextIndent + strings.Join(lineItems, ", ")
+			if i+itemsPerLine < len(arr) {
+				line += ","
+			}
+			lines = append(lines, line)
+		}
+
+		return "[\n" + strings.Join(lines, "\n") + "\n" + indent + "]"
+	}
+
+	var items []string
+	for _, item := range arr {
+		items = append(items, nextIndent+stringifyWithVisited(item, internal, visited, indentLevel+1))
+	}
+	return "[\n" + strings.Join(items, ",\n") + "\n" + indent + "]"
+}
+
+func stringifyWithVisited(value shared.RuntimeValue, internal bool, visited map[uintptr]bool, indentLevel int) string {
 	if value.Type == shared.Object || value.Type == shared.Array || value.Type == shared.ClassInstance {
 		if value.Value == nil {
 			return "null"
@@ -26,6 +120,7 @@ func stringifyWithVisited(value shared.RuntimeValue, internal bool, visited map[
 		defer delete(visited, ptr)
 	}
 
+	output := ""
 	switch value.Type {
 	case shared.String:
 		if internal {
@@ -44,26 +139,9 @@ func stringifyWithVisited(value shared.RuntimeValue, internal bool, visited map[
 	case shared.Nil:
 		output += "nil"
 	case shared.Object:
-		output += "{\r\n"
-		first := true
-		for key, val := range value.Value.(map[string]*shared.RuntimeValue) {
-			if !first {
-				output += ",\r\n"
-			}
-			first = false
-			output += fmt.Sprintf("\t%s: %s", key, stringifyWithVisited(*val, internal, visited)) + "\r\n"
-		}
-		output += "\r\n}"
+		output += formatObject(value.Value.(map[string]*shared.RuntimeValue), internal, visited, indentLevel)
 	case shared.Array:
-		arr := value.Value.([]shared.RuntimeValue)
-		output += "[\r\n"
-		for i, val := range arr {
-			if i > 0 {
-				output += ",\r\n"
-			}
-			output += "\t" + stringifyWithVisited(val, internal, visited)
-		}
-		output += "\r\n]"
+		output += formatArray(value.Value.([]shared.RuntimeValue), internal, visited, indentLevel)
 	case shared.Function:
 		output += "<function>"
 	case shared.NativeFN:
@@ -72,7 +150,6 @@ func stringifyWithVisited(value shared.RuntimeValue, internal bool, visited map[
 		output += "<class>"
 	case shared.ClassInstance:
 		output += "[ClassInstance]"
-
 	default:
 		output += fmt.Sprintf("Unknown - %+v", value.Value)
 	}
